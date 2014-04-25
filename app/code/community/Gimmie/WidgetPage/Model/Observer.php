0ad2e65d99bcf4cffe2452a3a14b35253d3a3520
Maythee Anegboonlap <null@llun.in.th>
2014-04-25 13:55:08 +0700

    Fixed new user event doesn't trigger.

--- app/code/community/Gimmie/WidgetPage/Model/Observer.php
+++ app/code/community/Gimmie/WidgetPage/Model/Observer.php
@@ -37,7 +37,7 @@ class Gimmie_WidgetPage_Model_Observer
     $pointsConfig = $this->getConfig('points');
 
     if ($generalConfig['gimmie_enable'] && $pointsConfig['gimmie_trigger_'.$event]) {
-      $id= Mage::getModel('core/cookie')->get('gmref');
+      $id= Mage::getModel('core/cookie')->get(self::COOKIE_KEY_SOURCE);
 
       $customerData = Mage::getModel('customer/customer')->load($id)->getData();
       $email = $customerData['email'];
